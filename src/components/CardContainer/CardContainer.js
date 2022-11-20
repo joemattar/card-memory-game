ce99870327f20210e03dbd6d589c8a204a089af9
Joe M <joemattar@gmail.com>
2022-11-20 11:17:24 -0500

    Implement random display of cards

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -7,20 +7,34 @@ function importAll(r) {
   return r.keys().map(r);
 }
 
-const images = importAll(
-  require.context("../../assets/pictures/cropped", false, /\.(jpeg)$/)
-);
-
 function CardContainer(props) {
+  const cardsImagesList = importAll(
+    require.context("../../assets/pictures/cropped", false, /\.(jpeg)$/)
+  );
+
+  const cardsInfoList = Object.values(cardsInfo);
+
+  function generateRandomArray(arrayLength) {
+    let sequentialArray = Array.from(Array(arrayLength).keys());
+    let randomArray = [];
+    while (sequentialArray.length > 0) {
+      const index = Math.floor(Math.random() * sequentialArray.length);
+      randomArray.push(sequentialArray[index]);
+      sequentialArray.splice(index, 1);
+    }
+    return randomArray;
+  }
+
   function displayCards() {
-    const cardsInfoList = Object.values(cardsInfo);
+    const randomArray = generateRandomArray(cardsInfoList.length);
+
     let cardsList = [];
-    for (let i = 0; i < cardsInfoList.length; i += 1) {
+    for (let i of randomArray) {
       const newCard = (
         <Card
           key={`key-${cardsInfoList[i].id}`}
           id={cardsInfoList[i].id}
-          url={images[i]}
+          url={cardsImagesList[i]}
           alt={cardsInfoList[i].shortname}
           name={cardsInfoList[i].name}
           shortname={cardsInfoList[i].shortname.toUpperCase()}
