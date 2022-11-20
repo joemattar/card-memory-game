536291bb31bc6cf183bb56531686d813181a0eb5
Joe M <joemattar@gmail.com>
2022-11-20 11:52:08 -0500

    Finalize scoring logic

--- src/App.js
+++ src/App.js
@@ -10,12 +10,28 @@ function App() {
 
   const [currentScore, setCurrentScore] = useState(0);
   const [highScore, setHighScore] = useState(0);
+  const [clickedCards, setClickedCards] = useState([]);
 
   function cardClickHandler(event) {
-    console.log(event.target);
-    console.log(event.target.parentNode);
+    // Grap target card node from click
+    let targetCard;
+    if (event.target.getAttribute("class") === "card") {
+      targetCard = event.target;
+    } else if (event.target.parentNode.getAttribute("class") === "card") {
+      targetCard = event.target.parentNode;
+    }
 
-    setCurrentScore(currentScore + 1);
+    console.log(targetCard.getAttribute("id"));
+
+    // If card is not already clicked, add to clicked cards array, increase current score
+    // If card already clicked, reset clicked cards array, reset current score
+    if (clickedCards.includes(targetCard.getAttribute("id")) === false) {
+      setClickedCards(clickedCards.concat(targetCard.getAttribute("id")));
+      setCurrentScore(currentScore + 1);
+    } else if (clickedCards.includes(targetCard.getAttribute("id")) === true) {
+      setClickedCards([]);
+      setCurrentScore(0);
+    }
   }
 
   function checkHighScore() {
